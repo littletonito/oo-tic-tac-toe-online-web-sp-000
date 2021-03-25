5cb106bc0d14be8aaaae9d874eebfb3d8df2c23f
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-25 16:17:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,14 +2,15 @@ require 'pry'
 
 
 class TicTacToe
-
-  WIN_COMBINATIONS = [
-    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
-  ]
-
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ]
 
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
@@ -19,49 +20,37 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index(input)
-    input.to_i - 1
+  def input_to_index(user_input)
+    user_input.to_i - 1
   end
 
-  def move(index, token = "X")
+  def move(index, token)
     @board[index] = token
   end
 
   def position_taken?(index)
-    if @board[index] == " "
-      false
-    else
-      true
-    end
+    @board[index] != " "
   end
 
   def valid_move?(index)
-    if @board[index] == " " && index.between?(0,8)
-      true
-    else
-      false
-    end
+    !position_taken?(index) && index.between?(0,8)
   end
 
   def turn_count
-    @board.count {|j| j == "X" || j == "O"}
+    @board.count{|square| square != " " }
   end
 
   def current_player
-    if turn_count.even?
-      "X"
-    else
-      "O"
-    end
+    turn_count.even? ? "X" : "O"
   end
 
   def turn
-    puts "Enter a number between 1-9"
-    input = gets.chomp
-    index = input_to_index(input)
+    puts "Please enter a number (1-9):"
+    user_input = gets.strip
+    index = input_to_index(user_input)
     if valid_move?(index)
-      move(index, token = "X")
-      current_player
+      token = current_player
+      move(index, token)
     else
       turn
     end
@@ -69,15 +58,15 @@ class TicTacToe
   end
 
   def won?
-    WIN_COMBINATIONS.any? do |j|
-      if position_taken?(j[0]) && @board[j[0]] == @board[j[1]] && @board[j[1]] == @board[j[2]]
-        return j
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
       end
     end
   end
 
   def full?
-    @board.all?{|j| j == "X" || j == "O" }
+    @board.all?{|square| square != " " }
   end
 
   def draw?
@@ -98,5 +87,4 @@ class TicTacToe
     turn until over?
     puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
   end
-
 end

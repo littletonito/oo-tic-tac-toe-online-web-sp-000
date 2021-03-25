a8aebea1bff61ab21ba589fa68c3b452502d80ee
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-25 16:08:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ class TicTacToe
 
   def won?
     WIN_COMBINATIONS.any? do |combo|
-      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] 
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
         return combo
       end
     end
